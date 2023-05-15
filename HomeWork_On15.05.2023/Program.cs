88de64dbfa6afbeb3b16b24b788b0eb652a31dab
DimaGit51 <Didvoryanchikov@yandex.ru>
2023-05-15 09:48:24 +0400

    Test

--- HomeWork_On15.05.2023/Program.cs
+++ HomeWork_On15.05.2023/Program.cs
@@ -49,9 +49,6 @@ namespace Program
     public class ArTree // Класс «Дерево арифметических выражений»
     {
         private ArNode root;
-        static public string math = "";
-        string xx = "";
-        string yy = "";
         public ArNode Root
         {
             get
@@ -105,7 +102,6 @@ namespace Program
                 Console.WriteLine();
             }
         }
-
         public void PrintLevel(ArNode root, int level)
         {
             if (root != null)
@@ -132,39 +128,63 @@ namespace Program
                 LKP(root.Right); // обойти правое поддерево в нисходящем порядке
             }
         }
-
-        public void Print(ArNode root) // root – ссылка на корень дерева и любого из деревьев
+        public void LPK(ArNode root) // root – ссылка на корень дерева и любого из деревьев
         {
             if (root != null) // дерево не пусто?
             {
-                Print(root.Left); // обойти левое поддерево в нисходящем порядке
-                if (root.Pr == 'n')
-                {
-                    if (xx.Length > 0) { yy = root.Info.ToString(); math += yy+")"; xx = ""; yy = ""; }
-                    else { xx = root.Info.ToString(); math += "(" + xx; }
-                }
-                else
-                {
-                    math += root.Pr;
-                }
-                Print(root.Right); // обойти правое поддерево в нисходящем порядке
+                LPK(root.Left); // обойти левое поддерево в нисходящем порядке
+                LPK(root.Right); // обойти правое поддерево в нисходящем порядке
+                if (root.Pr == 'n') Console.Write(root.Info + " ");
+                else Console.Write(root.Pr + " ");
             }
         }
-        
+        static public int Height(ArNode root, int count) // root – ссылка на корень дерева и любого из деревьев
+        {
+            if (root != null) // дерево не пусто?
+            {
+                count = Height(root.Left, count); // обойти левое поддерево в нисходящем порядке
+                count++;
+            }
+            return count;
+        }
+        public double ArTreeSumm(ArNode root)
+        {
+            double sum = 0;
+            if (root != null)
+            {
+                if (root.Left == null && root.Right == null)
+                    sum = root.Info;
+                double leftEval = ArTreeSumm(root.Left);
+                double rightEval = ArTreeSumm(root.Right);
+                if (root.Pr.Equals('+'))
+                    sum = leftEval + rightEval;
+                else if (root.Pr.Equals('-'))
+                    sum = leftEval - rightEval;
+                else if (root.Pr.Equals('*'))
+                    sum = leftEval * rightEval;
+                else if (root.Pr.Equals('/'))
+                    sum = leftEval / rightEval;
+            }
+            return sum;
+        }
     }
-    
-
     public class Program
     {
         static void Main()
         {
+            int n;
+            Console.Write("Введите Количество узлов = ");
+            while ((!int.TryParse(Console.ReadLine(), out n)))
+            {
+                Console.Write("Вы ввели не ^int^! Введите int: ");
+            }
             ArTree Ar = new ArTree();
-            Ar.Root = Ar.CreateRecursive(3);
-            Console.WriteLine("==========PRINT===========");
-            Ar.Print(Ar.Root);
-            string value = new DataTable().Compute(ArTree.math, null).ToString();
-            Console.WriteLine(value);
-
+            Ar.Root = Ar.CreateRecursive(n);
+            Console.WriteLine("==========PRINT Tree===========");
+            Ar.LPK(Ar.Root);
+            Console.WriteLine();
+            Console.WriteLine("==========PRINT SummTree===========");
+            Console.WriteLine(Ar.ArTreeSumm(Ar.Root));
         }
     }
 }
